2136771e4f22b5406e5033933b379e1b0c4ad75b
EXL <exlmotodev@gmail.com>
2024-07-25 05:13:52 +0700

    [brew_fire_effect]: Add bitmap generation

--- brew_fire_effect/brew_fire_effect.c
+++ brew_fire_effect/brew_fire_effect.c
@@ -14,7 +14,10 @@
  */
 
 #include <AEEAppGen.h>
+#include <AEEShell.h>
 #include <AEEStdLib.h>
+
+#include <AEEBitmap.h>
 
 #include "brew_fire_effect.bid"
 
@@ -25,6 +28,8 @@ typedef struct {
 	uint16 m_ScreenH;
 
 	AEERect m_ScreenRect;
+
+	IDIB *m_pIDIB;
 } APP_INSTANCE_T;
 
 static boolean APP_InitAppData(AEEApplet *pMe);
@@ -69,11 +74,35 @@ static boolean APP_InitAppData(AEEApplet *pMe) {
 
 	SETAEERECT(&app->m_ScreenRect, 0, 0, app->m_ScreenH, app->m_ScreenW);
 
+	if (IDISPLAY_CreateDIBitmap(app->m_App.m_pIDisplay, &app->m_pIDIB, 16, app->m_ScreenW, app->m_ScreenH)) {
+		return FALSE;
+	}
+	app->m_pIDIB->nColorScheme = IDIB_COLORSCHEME_565;
+
 	return TRUE;
 }
 
 static boolean APP_FreeAppData(AEEApplet *pMe) {
 	APP_INSTANCE_T *app = (APP_INSTANCE_T *) pMe;
+
+	IDIB_Release(app->m_pIDIB);
+
+	return TRUE;
+}
+
+static inline byte rand(void) {
+	byte random_number;
+	GETRAND((byte *) &random_number, sizeof(byte));
+	return random_number;
+}
+
+static boolean fill_test(AEEApplet *pMe) {
+	APP_INSTANCE_T *app = (APP_INSTANCE_T *) pMe;
+	int x = rand(), y;
+
+	for (y = 0; y <= app->m_ScreenH * app->m_ScreenW * 2; ++y) {
+		app->m_pIDIB->pBmp[y] = x;
+	}
 
 	return TRUE;
 }
@@ -90,7 +119,18 @@ static boolean APP_HandleEvent(AEEApplet *pMe, AEEEvent eCode, uint16 wParam, ui
 		case EVT_APP_STOP:
 			return TRUE;
 		case EVT_KEY:
-			ISHELL_CloseApplet(app->m_App.m_pIShell, FALSE);
+			switch (wParam) {
+				case AVK_SOFT1:
+					ISHELL_CloseApplet(app->m_App.m_pIShell, FALSE);
+				case AVK_SOFT2:
+					fill_test(pMe);
+					IDISPLAY_BitBlt(app->m_App.m_pIDisplay,
+						0, 0, app->m_ScreenW, app->m_ScreenW,
+						IDIB_TO_IBITMAP(app->m_pIDIB), 0, 0, AEE_RO_COPY);
+					IDISPLAY_Update(app->m_App.m_pIDisplay);
+				default:
+					break;
+			}
 			return TRUE;
 		case EVT_KEY_PRESS:
 			break;
